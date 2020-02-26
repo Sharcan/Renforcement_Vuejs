1938551b89d06c5a28a9cb84e7d88ca97b275b3c
Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2020-02-26 14:52:07 +0100

    Add Vue project

---
+++ yt_2.0/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+
+    <MainYt></MainYt>
+
+  </v-app>
+</template>
+
+<script>
+import MainYt from './components/MainYt';
+
+export default {
+  name: 'App',
+
+  components: {
+    MainYt
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
